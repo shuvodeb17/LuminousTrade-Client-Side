d42fbb0521995c12bc0afc36a5e0e748ced2b9ad
Shuvo Deb <shuvodeb17014@gmail.com>
2023-10-28 02:38:31 +0600

    added footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
+import Footer from "../components/Share/Footer/Footer";
 import Navbar from "../components/Share/Navbar/Navbar";
 
 const Main = () => {
     return (
         <div>
             <Navbar />
-            <Outlet/>
+            <Outlet />
+            <Footer />
         </div>
     );
 };
