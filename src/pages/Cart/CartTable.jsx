b914ba7792fff7bf97fe0f1f7e4584392ad36dc7
Shuvo Deb <shuvodeb17014@gmail.com>
2023-10-30 23:46:34 +0600

    click delete button delete product and immedietly refetch in ui

--- src/pages/Cart/CartTable.jsx
+++ src/pages/Cart/CartTable.jsx
@@ -1,8 +1,48 @@
 import { baseUrl } from "../../URL/URL";
+import { AiTwotoneDelete } from 'react-icons/ai';
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import { useMutation, useQueryClient } from 'react-query';
+import toast, { Toaster } from 'react-hot-toast';
+
+
 
 const CartTable = ({ product }) => {
     // console.log(product)
-    const { image, name, productName, price } = product;
+    const { user } = useContext(AuthContext)
+    const { _id, image, name, productName, price } = product;
+
+
+    // delete cart data start here
+    const queryClient = useQueryClient();
+    const deleteProduct = async (id) => {
+        const response = await fetch(`${baseUrl}/cart-delete-product/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to delete product');
+        }
+
+        return response.json();
+    };
+
+    const { isLoading, mutate, refetch } = useMutation(deleteProduct, {
+        onSettled: () => {
+            queryClient.invalidateQueries(['user', user?.email]);
+        },
+    });
+
+    const deleteHandler = (id) => {
+        mutate(id);
+        refetch
+        toast.success('Product Delete Successful')
+    };
+    // delete cart data end here
+
+
+
+
     return (
         <tr class="text-gray-700">
             <td class="px-4 py-3 border">
@@ -21,7 +61,12 @@ const CartTable = ({ product }) => {
             <td class="px-4 py-3 text-xs border">
                 <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> {price} </span>
             </td>
-            <td class="px-4 py-3 text-sm border">Delete</td>
+            <td class="px-4 py-3 text-sm border">
+                <button onClick={() => deleteHandler(_id)} className="cursor-pointer bg-red-400 text-[#2b2a2a] rounded-full p-1">
+                    <AiTwotoneDelete size={24} />
+                </button>
+            </td>
+            <Toaster />
         </tr>
     );
 };
